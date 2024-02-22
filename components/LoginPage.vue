c1586361e2c83150e92d2a545def217a2e7c7bb7
zz41d <141715534+zz41d@users.noreply.github.com>
2024-02-22 17:34:57 +0530

    LoginPage.vue

--- components/LoginPage.vue
+++ components/LoginPage.vue
@@ -1,28 +1,34 @@
-
-<template>
-    <div>
-      <h1>Login</h1>
-      <form @submit="login">
-        <input type="email" v-model="email" placeholder="Email" required>
-        <input type="password" v-model="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-      </form>
+  <template>
+    <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div class="bg-white p-8 rounded-lg shadow-md w-80">
+        <h1 class="text-2xl font-semibold mb-4">Login</h1>
+        <form @submit.prevent="login">
+          <input type="text" v-model="username" placeholder="Username" required class="block w-full px-4 py-2 border border-gray-300 rounded-md mb-2 focus:outline-none focus:border-blue-400">
+          <input type="password" v-model="password" placeholder="Password" required class="block w-full px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-400">
+          <button type="submit" @click.prevent="log(username,password)" class="bg-green-500 text-white px-4 py-2 rounded-md w-full">Login</button>
+        </form>
+      </div>
     </div>
   </template>
+    
   
   <script setup>
-  const email = ref('');
-  const password = ref('');
-  
-  const login = () => {
-    const  isLoggedIn=false
-    if(!isLoggedIn && to.path!=='/login'){
-        return navigateTo("/login")
-    }
-    else if(isLoggedIn && to.path==='/login')
-        
-    return navigateTo("/")
+  import { useSSRContext } from 'vue';
 
+const {login}=useNuxtApp()
+const users=usersList()
+  const username = ref('');
+  const password = ref('');
+  const loginStatus=isLoggedIn()
+  const log= (username,password) => {
+        let a=login(username,password)
+    console.log(a)
+    loginStatus.value=true
+    if(loginStatus){
+        navigateTo('/')
+    }
   };
+  definePageMeta({
+    middleware:["auth"],
+})
   </script>
-  
