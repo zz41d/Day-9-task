3bcac7da4e354034831295c1caec4b2a901bf7ea
zz41d <141715534+zz41d@users.noreply.github.com>
2024-02-22 17:42:55 +0530

    about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,15 @@
     <div>
       <h2>About</h2>
       <p>This page is all about Nuxt.js</p>
-      <p>By using components, pages, and layouts this pages have been created. Also we're using definePageMeta() composable function and defineProps().</p>
+      <p>By using components, pages, and layouts this pages have been created. Also we're using definePageMeta() composable function and defineProps(). 
+        With the help of middleware and plugins we're logging in to the web page and if there is new user then they've to register</p>
     </div>
 </template>
 
 <script setup>
+  definePageMeta({
+    middleware:["auth"]
+})
 
 </script>
 
