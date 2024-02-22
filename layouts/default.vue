10e251ba481917f47e9653a7054b2e117395df4d
zz41d <141715534+zz41d@users.noreply.github.com>
2024-02-22 17:36:48 +0530

    default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,17 +9,23 @@
                 <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
                 <li><NuxtLink to="/login">Login</NuxtLink></li>
                 <li><NuxtLink to="/register">Register </NuxtLink></li>
-
-
             </ul>
         </nav>
       </header>
-      </div>
+     </div>
       <!-- output content -->
       <div class="container mx-auto p-4">
         <slot/>
       </div>
 </template>
+<script setup>
+ import { useSSRContext } from 'vue';
+  const {login}=useNuxtApp()
+ const users=usersList()
+ definePageMeta({
+    middleware:["auth"]
+})
+</script>
 <style scoped>      
   .router-link-exact-active {
     color: #32803b;
